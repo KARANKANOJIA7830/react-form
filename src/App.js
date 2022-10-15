c86c4c813faf46ffefe43e4456a42e924ef7cfa7
Karan Kanojia <86140822+KARANKANOJIA7830@users.noreply.github.com>
2022-10-15 19:09:02 +0530

    changes updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [fullName, setFullName] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        phone: "",
+        msg: "",
+    });
+
+
+    const inputEvent = (event) => {
+        console.log(event.target.value);
+        console.log(event.target.name);
+
+        const {name, value } = event.target;
+
+        setFullName((preValue) => {
+            console.log(preValue);
+
+            return {
+                ...preValue,
+                [name] : value,
+            }
+
+            
+            // if (name === 'fName') {
+            //     return {
+            //         fname: value,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phone: preValue.phone,
+            //     };
+            // } else if (name === 'lName') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: value,
+            //         email: preValue.email,
+            //         phone: preValue.phone,
+            //     };
+            // } else if (name === 'email') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: value,
+            //         phone: preValue.phone,
+            //     };
+            // } else if (name === 'phone') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phone: value,
+            //     };
+            // }
+        });
+    };
+
+
+    const onSubmits = (event) => {
+        event.preventDefault();
+        alert("THANKYOU form submitted ✨");
+    };
+
+
+    return (
+        <>
+            <div className="main_div">
+                <form onSubmit={onSubmits}>
+                    <div>
+                        <h1>Hello {fullName.fname} {fullName.lname}</h1>
+                        <p> {fullName.email}</p>
+                        <p> {fullName.phone}</p>
+                        <p> {fullName.msg}</p>
+                        <input
+                            type="text" placeholder="Enter your First Name"
+                            name="fname"
+                            onChange={inputEvent}
+                            value={fullName.fname}
+                        />
+                        <input
+                            type="text" placeholder="Enter your Last Name"
+                            name="lname"
+                            onChange={inputEvent}
+                            value={fullName.lname}
+                        />
+                        <input
+                            type="email" placeholder="Enter your email id"
+                            name="email"
+                            onChange={inputEvent}
+                            value={fullName.email}
+                            autoComplete='off'
+                        /><input
+                            type="number" placeholder="Enter your phone number"
+                            name="phone"
+                            onChange={inputEvent}
+                            value={fullName.phone}
+                        />
+                        <input
+                            type="text" placeholder="Enter your message"
+                            name="msg"
+                            onChange={inputEvent}
+                            value={fullName.msg}
+                        />
+                        <button type="submit">Submit🤝</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
 
 export default App;
